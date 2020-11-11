f0bc219436855b2ff29f20f169ddeb37344c09c7
Nguyễn Văn Quang <73304614+QuanghustET1@users.noreply.github.com>
2020-11-11 15:17:06 +0700

    Add files via upload

---
+++ Image_to_Ascii.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import time
+
+# ascii characters used to build the output text
+ASCII_CHARS = ["#", "%", "@", "&", ":", ",", ",", ";", ".", "*", ";"]
+print("----------------------------- Copyright by Quang Hust-----------------------------")
+# resize image according to a new width
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height/width
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return(resized_image)
+
+# convert each pixel to grayscale
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
+    
+# convert pixels to a string of ascii characters
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)    
+
+def main(new_width=100):
+    # attempt to open image from user-input
+    path = input("Enter the name of image: ")
+    
+    try:
+        image = Image.open(path)
+        time.sleep(4)
+    except:
+        print("Not Found file "+path+"in this Folder")
+        time.sleep(5)
+  
+    # convert image to ascii    
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+    
+    # format
+    pixel_count = len(new_image_data)  
+    ascii_image = "\n".join([new_image_data[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
+    
+    # print result
+    print(ascii_image)
+    
+    # save result to "ascii_image.txt"
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
+time.sleep(5)
+# run program
+main()
